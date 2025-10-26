ac7ad405a0b70580b4f074799ba7565cb8d2832c
LuisJimenezJordan <luisjimenezljimbol@gmail.com>
2025-10-26 13:01:42 +0100

    Update limited_login_attempts.py

--- Week 1/limited_login_attempts.py
+++ Week 1/limited_login_attempts.py
@@ -1,10 +1,10 @@
-correct_pin = "1234"
+correct_pin = "9387"
 attempts = 0
-max_attempts = 3
+max_attempts = 5
 login_successful = False
 while attempts < max_attempts:
     print(f"Attempt {attempts + 1} of {max_attempts}")
-    entered_pin = input("Enter your PIN: ")
+    entered_pin = input("Enter your four digit PIN: ")
 
     if entered_pin == correct_pin:
         print("PIN accepted! Welcome.")
